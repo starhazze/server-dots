e7f3e04926fc231ab66ab3169ec03d654e6e01ed
starhazze <starhazze@proton.me>
2026-06-15 13:00:08 +0000

    refactor: modernize lemmy config

--- lemmy.nix
+++ lemmy.nix
@@ -1,7 +1,6 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, ... }:
 let
   lemmy = {
-    dataDir = "/var/lib/lemmy";
     ip = "127.0.0.1";
     port = 8536;
     domain = "lm.kluge.cafe";
@@ -18,13 +17,34 @@ in {
     address = pict-rs.ip;
   };
 
-  systemd.services.lemmy = {
-    requires = [ "postgresql.service" ];
-    after = [ "postgresql.service" ];
-    environment = {
-      LEMMY_DATABASE_URL = lib.mkForce "postgresql://lemmy@127.0.0.1:${toString config.services.postgresql.settings.port}/lemmy";
+  virtualisation.docker.enable = true;
+
+  virtualisation.oci-containers.backend = "docker";
+
+  virtualisation.oci-containers.containers = {
+    photon = {
+      image = "ghcr.io/xyphyn/photon:latest";
+      ports = [ "127.0.0.1:3003:3000" ];
+      environment = {
+        PUBLIC_INSTANCE_URL = lemmy.domain;
+        PUBLIC_SSR_ENABLED = "true";
+        PUBLIC_INTERNAL_INSTANCE = "${lemmy.ip}:${toString lemmy.port}";
+      };
+    };
+
+    blorp = {
+      image = "ghcr.io/blorp-labs/blorp:latest";
+      ports = [ "127.0.0.1:3002:80" ];
+      environment = {
+        REACT_APP_DEFAULT_INSTANCE = "https://${lemmy.domain}";
+        REACT_APP_LOCK_TO_DEFAULT_INSTANCE = "1";
+      };
     };
   };
+
+  services.postgresql.authentication = lib.mkAfter ''
+    host lemmy lemmy 127.0.0.1/32 trust
+  '';
 
   services.caddy.virtualHosts."lm.kluge.cafe".extraConfig = ''
     handle /verify_email* {
@@ -42,81 +62,10 @@ in {
     reverse_proxy 127.0.0.1:8536
   '';
 
-  services.caddy.virtualHosts."pf.kluge.cafe".extraConfig = ''
-    reverse_proxy /notifications* localhost:8040
-    reverse_proxy * localhost:8030
-  '';
 
-  services.caddy.virtualHosts."photon-pf.kluge.cafe".extraConfig = ''
-    reverse_proxy /notifications* localhost:8040
-    reverse_proxy * localhost:3004
-  '';
-
-  services.caddy.virtualHosts."blorp-pf.kluge.cafe".extraConfig = ''
-    reverse_proxy /notifications* localhost:8040
-    reverse_proxy * localhost:3005
-  '';
-
-  systemd.services.lemmy-ui = {
-    environment = {
-      LEMMY_UI_HOST = lib.mkForce "${lemmy.ip}:${toString config.services.lemmy.ui.port}";
-      LEMMY_DATABASE_URL = lib.mkForce "postgresql://lemmy@localhost:${toString config.services.postgresql.settings.port}/lemmy";
-      LEMMY_UI_LEMMY_INTERNAL_HOST = lib.mkForce "${lemmy.ip}:${toString lemmy.port}";
-      LEMMY_UI_LEMMY_EXTERNAL_HOST = lib.mkForce lemmy.domain;
-      LEMMY_UI_HTTPS = lib.mkForce "true";
-    };
-  };
-
-  virtualisation.docker.enable = true;
-  
-  virtualisation.oci-containers.backend = "docker";
-  
-  virtualisation.oci-containers.containers = {
-    photon = {
-      image = "ghcr.io/xyphyn/photon:latest";
-      ports = [ "127.0.0.1:3003:3000" ];
-      environment = {
-        PUBLIC_INSTANCE_URL = "lm.kluge.cafe";
-        PUBLIC_SSR_ENABLED = "true";
-        PUBLIC_INTERNAL_INSTANCE = "127.0.0.1:8536";
-      };
-    };
-
-    blorp = {
-      image = "ghcr.io/blorp-labs/blorp:latest";
-      ports = [ "127.0.0.1:3002:80" ];
-      environment = {
-        REACT_APP_DEFAULT_INSTANCE = "https://lm.kluge.cafe";
-        REACT_APP_LOCK_TO_DEFAULT_INSTANCE = "1";
-      };
-    };
-  };
-
-  services.postgresql.authentication = lib.mkAfter ''
-    host lemmy lemmy 127.0.0.1/32 trust
-  '';
-
-  users.users.lemmy = {
-    isSystemUser = true;
-    group = "lemmy";
-  };
-  
-  users.groups.lemmy = {};
-
-  age.secrets.lemmy-smtp = {
-    file = ./secrets/lemmy-smtp.age;
-    owner = "lemmy";
-  };
-
-  age.secrets.pictrs-api-key = {
-    file = ./secrets/pictrs-api-key.age;
-    owner = "lemmy";
-  };
-  
-  age.secrets.lemmy-admin-pass = {
-    file = ./secrets/lemmy-admin-pass.age;
-    owner = "lemmy";
-  };
+  age.secrets.lemmy-smtp.file = ./secrets/lemmy-smtp.age;
+  age.secrets.pictrs-api-key.file = ./secrets/pictrs-api-key.age;
+  age.secrets.lemmy-admin-pass.file = ./secrets/lemmy-admin-pass.age;
 
   services.lemmy = {
     enable = true;
@@ -133,19 +82,9 @@ in {
       tls_enabled = true;
       email = {
         smtp_server = "smtp-relay.brevo.com:587";
-	smtp_login = "a2878c001@smtp-brevo.com";
-	smtp_from_address = "noreply@kluge.cafe";
-	tls_type = "none";
-      };
-      database = {
-        user = "lemmy";
-        host = "localhost";
-        port = config.services.postgresql.settings.port;
-        database = "lemmy";
-        pool_size = 5;
-      };
-      pictrs = {
-        url = "http://${pict-rs.ip}:${toString pict-rs.port}/";
+        smtp_login = "a2878c001@smtp-brevo.com";
+        smtp_from_address = "noreply@kluge.cafe";
+        tls_type = "none";
       };
       setup = {
         admin_username = "admin";
@@ -153,13 +92,4 @@ in {
       };
     };
   };
-
-  system.activationScripts."make_sure_lemmy_user_owns_files" = ''
-    dir='${lemmy.dataDir}'
-    mkdir -p "''${dir}"
-  
-    if [[ "$(${pkgs.toybox}/bin/stat "''${dir}" -c '%U:%G' | tee /dev/stderr)" != "lemmy:lemmy" ]]; then
-      chown -R lemmy:lemmy "''${dir}"
-    fi
-  '';
 }
